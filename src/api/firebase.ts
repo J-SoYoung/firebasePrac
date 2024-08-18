9901796b28e025ba4dd35d76834cc185ebd5e8e0
thdud <thgml1444@naver.com>
2024-08-18 13:36:44 +0900

    Add : firebase의 setting파일에 auth 설정코드 추가

--- src/api/firebase.ts
+++ src/api/firebase.ts
@@ -1,10 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: import.meta.env.VITE_APP_FIREBASE_DATABASE_URL,
   projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
@@ -14,5 +19,11 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Realtime Database and get a reference to the service
-export const database = getDatabase(app);
+// Initialize Realtime Database
+const database = getDatabase(app);
+
+// Initialize auth
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export { database, auth, provider, signInWithPopup, signOut };
