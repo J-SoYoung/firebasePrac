8a2af325ad076f90449ddfbc5d1dc8126ac47914
thdud <thgml1444@naver.com>
2024-08-18 13:35:50 +0900

    Add : apis.ts 파일에 있던 auth관련 함수를 따로 만들었다. - firebase에는 많은 함수가 생길 예정이므로 보기 편하게 로그인 함수만 빼두었다.

---
+++ src/api/auth.ts
@@ -0,0 +1,23 @@
+import { auth, provider, signInWithPopup, signOut } from "./firebase";
+
+export const signInWithGoogle = async () => {
+  try {
+    const result = await signInWithPopup(auth, provider);
+    const user = result.user;
+    console.log("User info:", user);
+    return user;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const signOutFromGoogle = async () => {
+  try {
+    await signOut(auth);
+    console.log("User signed out");
+  } catch (error) {
+    console.error("Sign-Out Error:", error);
+    throw error;
+  }
+};
+
